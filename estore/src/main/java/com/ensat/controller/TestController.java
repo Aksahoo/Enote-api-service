6d22bd0464bd3ccfb59b0d813bab5d2e1fcc63db
Aksahoo <ak04java@gmail.com>
2025-07-29 11:38:34 +0530

    tyuuioo

--- estore/src/main/java/com/ensat/controller/TestController.java
+++ estore/src/main/java/com/ensat/controller/TestController.java
@@ -3,6 +3,11 @@ package com.ensat.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-
+public class TestController{
+	
+	
+	public String test() {
+		return null;
+	
+	}
 }
